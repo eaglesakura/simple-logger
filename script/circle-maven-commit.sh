5d72367d17d7b65f78baa41d2a503a43992f2608
eaglesakura <eagle.sakura@gmail.com>
2016-02-08 19:51:25 +0900

    if文が正常に動作しない不具合を修正

--- script/circle-maven-commit.sh
+++ script/circle-maven-commit.sh
@@ -4,11 +4,12 @@
 git config --global user.email 'eagle.sakura+deploy@gmail.com'
 git config --global user.name 'eaglesakura-deploy'
 
-if [ -e ./maven ]; then {
+if [ -e ./maven ]; then
     echo "maven directory notfound..."
     exit 1
-}
+fi
 
 cd maven
 git add .
-git commit -am "[deploy] add $CIRCLE_PROJECT_REPONAME"
+git commit -am "[deploy] add $CIRCLE_PROJECT_REPONAME $CIRCLE_BRANCH"
+git push origin gh-pages
