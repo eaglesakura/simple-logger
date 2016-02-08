231c518b7cb09e2978606cd06c26ec4d430f11ec
eaglesakura <eagle.sakura@gmail.com>
2016-02-08 19:54:40 +0900

    ディレクトリチェックの対象が間違っていたので修正

--- script/circle-maven-commit.sh
+++ script/circle-maven-commit.sh
@@ -5,11 +5,13 @@ git config --global user.email 'eagle.sakura+deploy@gmail.com'
 git config --global user.name 'eaglesakura-deploy'
 
 if [ -e ./maven ]; then
+    echo "maven directory found. commit -> maven $CIRCLE_PROJECT_REPONAME $CIRCLE_BRANCH"
+else
     echo "maven directory notfound..."
     exit 1
 fi
 
 cd maven
 git add .
-git commit -am "[deploy] add $CIRCLE_PROJECT_REPONAME $CIRCLE_BRANCH"
+git commit -am "[Circle-CI] add $CIRCLE_PROJECT_REPONAME $CIRCLE_BRANCH"
 git push origin gh-pages
